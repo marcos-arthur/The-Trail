cca675ae60bd0601f760f0e4f3e2ff7808358e62
marcos-arthur <marcos.marcos.arthur291@gmail.com>
2023-10-17 20:44:05 -0300

    Adição de bordas e ícone do jogo

--- scripts/NPC/NPCController.gd
+++ scripts/NPC/NPCController.gd
@@ -3,8 +3,6 @@ class_name NPCSpriteController
 
 @export var animation: AnimationPlayer
 
-@onready var NPCController = get_node("npc_controller")
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	animation.play("Idle")
